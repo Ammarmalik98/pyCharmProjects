1004db63525a96838e39d992daf14f9882429c54
ammar_malik <ammarelmalik@gmail.com>
2025-04-13 21:21:31 +0100

    Second entry

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("How do you do?")
+first_name = "Ammar"
+email = "ammarmal33k@gmail.com"
+fav_food = "Pastries"
+
+print(
+    f"Welcome {first_name}",
+    f"your email is --> {email}",
+    f"and your favourite food is {fav_food}"
+)
