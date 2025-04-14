f31a46b9a05357b2cca87f5cdd59616d02f05480
ammar_malik <ammarelmalik@gmail.com>
2025-04-14 21:15:16 +0100

    Madlibs Game. Built using input functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 first_name = "Ammar"
 email = "ammarmal33k@gmail.com"
 fav_food = "Pastries"
@@ -7,3 +8,32 @@ print(
     f"your email is --> {email}",
     f"and your favourite food is {fav_food}"
 )
+"""
+
+#name= input("What is your name? ")
+#age= input("how old are you? ")
+
+#print(f"Hello {name}, nice to meet you...{age} years huh? we seem to be around the same age")
+
+"""
+item = input("What would you like to buy?: ")
+price = float(input("How much des it cost?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} {item}s")
+print(f"Your total expenditure is ${total}")
+"""
+
+# Practice Input variables with Madlibs game
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun (person, animal, place or thing); ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb ending with -ing: ")
+adjective3 = input("Enter an adjective: ")
+
+print(f"Today i went to an {adjective1} zoo.")
+print(f"In an exhibit, i saw a {noun1}")
+print(f"The {noun1} was {verb1} at me {adjective2}")
+print(f"I was {adjective3}!")
