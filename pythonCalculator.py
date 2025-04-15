d861bdfa0af56eda14fdab717da3390b21e9c23e
ammar_malik <ammarelmalik@gmail.com>
2025-04-15 09:43:30 +0100

    Basic Python Arithmetic operations

--- pythonCalculator.py
+++ pythonCalculator.py
@@ -1,5 +1,5 @@
 # Python Calculator
-
+"""
 operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
@@ -18,3 +18,20 @@ elif operator == "/":
     print(round(result, 2))
 else:
     print(f"{operator} is not a valid operator")
+"""
+
+weight = float(input("enter your weight "))
+unit = input("kilograms or pounds? (K or L): ")
+
+
+if unit == "K":
+    weight *= 2.205
+    unit = "Lbs."
+elif unit == "L":
+    weight /= 2.205
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid")
+
+print (f"Your weight is: {round(weight, 1)} {unit}")
+
